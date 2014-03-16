cc0f355cee1b06780b9541b1456646e36f71a60c
Matheus Moreira <mvrmoreira@gmail.com>
2014-03-16 20:32:24 -0300

    generating instance crud

---
+++ src/Uff/CalculatorBundle/Form/InstanceType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Uff\CalculatorBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class InstanceType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('gflops')
+            ->add('ram')
+            ->add('price')
+            ->add('category')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Uff\CalculatorBundle\Entity\Instance'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'uff_calculatorbundle_instance';
+    }
+}
