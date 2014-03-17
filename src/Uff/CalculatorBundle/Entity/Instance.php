d97a1cacfd8de9eef84e5bf08976ad6eec1fefd1
Matheus Moreira <mvrmoreira@gmail.com>
2014-03-16 21:11:15 -0300

    adjusting decimal fields

--- src/Uff/CalculatorBundle/Entity/Instance.php
+++ src/Uff/CalculatorBundle/Entity/Instance.php
@@ -58,7 +58,7 @@ class Instance
     /**
      * @var string
      *
-     * @ORM\Column(name="price", type="decimal", scale=2))
+     * @ORM\Column(name="price", type="decimal", scale=2)
      */
     private $price;
 
